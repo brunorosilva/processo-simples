2ab77fb008882f96b9b0d9591e82262c16cb8a13
bevilacqua1996 <brunoenig@hotmail.com>
2019-12-03 01:09:53 -0300

    Histórico
    Implementação da inserção dinâmica dos dados de histórico.

--- app_cliente/js/historico_processo.js
+++ app_cliente/js/historico_processo.js
@@ -1,3 +1,5 @@
+const timeoutCreationHtmlListHistoric = 500;
+
 function goToProcesso() {
     window.location.assign('../html/detalhamento_processo.html?processo=' + getProcesso());
 }
@@ -12,4 +14,38 @@ function buildSubTitleOnHistoricPage() {
     var url = new URL(window.location.href);
     h3.innerHTML = "Processo: " + url.searchParams.get("processo");
     document.getElementById("processo").appendChild(h3);
+
+    getJsonHistoricoProcesso();
+}
+
+function buildListHtml(dados); {
+    setTimeout(function() {
+
+        var liTag = 
+        `<li>
+            <a target="_blank" href="#">Publicação extraída JusBrasil</a>
+            <a href="#" class="float-right">${dados["data"]}</a>
+            <p>Dados essenciais do processo na data apontada</p>
+            <p>ANDAMENTO:${dados["andamento"]}</p>
+        </li>`;
+
+        var divLiTag = document.createElement("div");
+        divLiTag.innerHTML = liTag;
+        document.getElementsByClassName("timeline").appendChild(divLiTag);
+
+    }, timeoutCreationHtmlListHistoric)
+}
+
+function getJsonHistoricoProcesso() {
+    fetch('https://app-processa-ai.herokuapp.com/api/Processo/historico/numero_processo/' + getProcesso(), {
+            credentials: "omit"
+        })  
+        .then(response => response.json())
+        .then(data => {
+            var dados = data["data"];
+            for(i in dados) {
+                buildListHtml(dados[i]);
+            }
+        })
+        .catch(error => console.error(error));
 }
